fe9a4c84418ccfa3cd52e7958ae1a0b2895866fe
unknown <you@example.com>
2019-10-28 06:46:03 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -5,13 +5,30 @@ $(function () {
     fade: true,
     prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt="Prev">',
     nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="Next">',
-    asNavFor: '.slider-dots'
+    asNavFor: '.slider-dots__head'
   });
 
-  $('.slider-dots').slick({
+  $('.slider-dots__head').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     asNavFor: '.header__slider',
+    focusOnSelect: true
+  });
+
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerMode: true,
+    prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt="Prev">',
+    nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="Next">',
+    asNavFor: '.slider-map'
+  });
+
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf-slider',
     focusOnSelect: true
   });
 
