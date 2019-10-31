0737cdf1e90d7e7c268dcf0af1db5a03dbc12349
unknown <you@example.com>
2019-10-31 07:10:04 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -32,4 +32,11 @@ $(function () {
     focusOnSelect: true
   });
 
+  $('.travel__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/arrow-left.svg" alt="Prev">',
+    nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/arrow-right.svg" alt="Next">',
+  });
+
 });
